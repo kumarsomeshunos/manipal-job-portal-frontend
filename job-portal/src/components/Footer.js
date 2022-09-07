003e806f539039dc5a6e56087162214f30bd5b8e
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2022-09-08 00:01:53 +0530

    Navbar+Footer commit

---
+++ job-portal/src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer class='footer'>
+      <div class='container-2'>
+        <div class='row2'>
+          <div class='row'>
+            <div class='column'>
+              <ul class='list-unstyled'>
+                <li>
+                  <a href=''>Product</a>
+                </li>
+                <li>
+                  <a href=''>Benefits</a>
+                </li>
+                <li>
+                  <a href=''>Team</a>
+                </li>
+              </ul>
+            </div>
+            <div class='column'>
+              <ul class='list-unstyled'>
+                <li>
+                  <a href=''>Product</a>
+                </li>
+                <li>
+                  <a href=''>Benefits</a>
+                </li>
+
+                <li>
+                  <a href=''>Team</a>
+                </li>
+              </ul>
+            </div>
+            <div class='column'>
+              <ul class='list-unstyled'>
+                <li>
+                  <a href=''>Product</a>
+                </li>
+                <li>
+                  <a href=''>Benefits</a>
+                </li>
+
+                <li>
+                  <a href=''>Team</a>
+                </li>
+              </ul>
+            </div>
+            <div class='column'>
+              <ul class='list-unstyled'>
+                <li>
+                  <a href=''>Product</a>
+                </li>
+                <li>
+                  <a href=''>Benefits</a>
+                </li>
+
+                <li>
+                  <a href=''>Team</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
